71082cce42bd0eb0895e72c9a66e2d09e076b212
Josh Cardenzana <jcardenzana@irap.omp.eu>
2018-05-31 22:43:23 +0200

    Update engine to reflect addition of avatar selection option

--- js/engine.js
+++ js/engine.js
@@ -32,25 +32,27 @@ var Engine = (function(global) {
      * and handles properly calling the update and render methods.
      */
     function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
+        if (avatarSelected) {
+            /* Get our time delta information which is required if your game
+            * requires smooth animation. Because everyone's computer processes
+            * instructions at different speeds we need a constant value that
+            * would be the same for everyone (regardless of how fast their
+            * computer is) - hurray time!
+            */
+            var now = Date.now(),
+                dt = (now - lastTime) / 1000.0;
 
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-        update(dt);
-        render();
+            /* Call our update/render functions, pass along the time delta to
+            * our update function since it may be used for smooth animation.
+            */
+            update(dt);
+            render();
 
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
+            /* Set our lastTime variable which is used to determine the time delta
+            * for the next time this function is called.
+            */
+            lastTime = now;
+        }
 
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
@@ -108,11 +110,11 @@ var Engine = (function(global) {
          */
         var rowImages = [
                 'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/grass-block.png',   // Row 1 of 3 of stone
+                'images/grass-block.png',   // Row 2 of 3 of stone
+                'images/grass-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/stone-block.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -156,12 +158,85 @@ var Engine = (function(global) {
         player.render();
     }
 
+    /* This function initially draws the "avatar selection", it will then call
+     * the renderAvatars function.
+     */
+    function renderSelection() {
+        /* This array holds the relative URL to the image used
+         * for that particular row of the game level.
+         */
+        var rowImages = [
+                'images/water-block.png',   // Top row is water
+                'images/grass-block.png',   // Row 1 of 3 of stone
+                'images/grass-block.png',   // Row 2 of 3 of stone
+                'images/grass-block.png',   // Row 3 of 3 of stone
+                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/stone-block.png'    // Row 2 of 2 of grass
+            ],
+            numRows = 6,
+            numCols = 5,
+            row, col;
+        
+        // Before drawing, clear existing canvas
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+
+        /* Loop through the number of rows and columns we've defined above
+         * and, using the rowImages array, draw the correct image for that
+         * portion of the "grid"
+         */
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                /* The drawImage function of the canvas' context element
+                 * requires 3 parameters: the image to draw, the x coordinate
+                 * to start drawing and the y coordinate to start drawing.
+                 * We're using our Resources helpers to refer to our images
+                 * so that we get the benefits of caching these images, since
+                 * we're using them over and over.
+                 */
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }
+
+        renderAvatars();
+
+        // Set the players image and keep going
+        if (avatarSelected) {
+            player.sprite = selectedAvatar;
+            player.reset();
+            return;
+        }
+
+        win.requestAnimationFrame(renderSelection);
+    }
+
+    /* This function is called by the renderSelection function. Its purpose is 
+     * to call the render functions that have been defined for avatar selection
+     */
+    function renderAvatars() {
+
+        // Render the selection object
+        selector.y = 375;
+        selector.render();
+
+        /* Loop through all of the objects within the allAvatars array and call
+         * their render functions.
+         */
+        allAvatars.forEach(function(avatar) {
+            avatar.render();
+            avatar.fixed = true;
+        });
+    }
+
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+        // Set the instructions at the bottom
+        document.getElementById("score").innerHTML = `Please select your avatar`;
+
+        // Load a screen for selecting the player avatar
+        renderSelection();
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -173,7 +248,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Selector.png'
     ]);
     Resources.onReady(init);
 

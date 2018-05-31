f9e7ab6c7ff16e14bde7d6a405dd02ae2363954a
Josh Cardenzana <jcardenzana@irap.omp.eu>
2018-05-31 10:12:05 +0200

    Add game functionality. A basic version of the game should now be playable Everything should actually work now.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,124 @@
+var ystep  = 83;
+var xstep = 100;
+
+// Base class for all objects that need a sprite
+var Sprite = function() {
+    this.sprite = 'images/star.png';
+    this.x = 0;
+    this.y = 0;
+};
+
+// Default to doing nothing
+Sprite.prototype.update = function() {
+    
+};
+
+// Draw the enemy on the screen, required method for game
+Sprite.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.y = 400 - y*ystep;
+    this.x = -101;
+    this.width = 50;
+    this.velocity = 50 + 100*Math.random();
+    this.reset();
 };
+Enemy.prototype = Object.create(Sprite.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply any movement by the dt parameter which will
+    // ensure the game runs at the same speed for all computers.
+    this.x = this.x + this.velocity * dt;
+    if ((this.x > 501)||(this.x<-101)) {
+        this.reset();
+    }
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Reset the enemy character
+Enemy.prototype.reset = function() {
+    //this.x = -101;
+    this.velocity = 100 + 200*Math.random();
+    if (this.x > 400) {
+        this.velocity *= -1;
+    }
+}
 
-// Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.reset();
+};
+Player.prototype = Object.create(Sprite.prototype);
+Player.prototype.constructor = Player;
 
+// Move the player around
+Player.prototype.handleInput = function(key) {
+    
+    // Move left
+    if ((key === 'left') && (this.x > 1)) {
+        this.x -= xstep;
+    }
+    // Move right
+    else if ((key === 'right') && (this.x < 400)) {
+        this.x += xstep;
+    } 
+    // Move up
+    else if ((key === 'up') && (this.y > 0)) {
+        this.y -= ystep;
+    } 
+    // Move down
+    else if ((key === 'down') && (this.y < 400)) {
+        this.y += ystep;
+    }
+};
+
+Player.prototype.reset = function() {
+    this.x = 201;
+    this.y = 400;
+}
+
+
+// The player update method that is responsible for detecting collisions
+// and determining if the player has reached the end
+Player.prototype.update = function() {
+    // Loop over the enemies and see if we have any collisions
+    if (this.y === -15) {
+        this.reset();
+    }
+    for (const enemy of allEnemies) {
+        if ((this.y === enemy.y) &&
+            (Math.abs(enemy.x - this.x) < 70)) {
+            this.reset();
+        }
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy(2);
+var enemy2 = new Enemy(3);
+var enemy3 = new Enemy(4);
+var allEnemies = [
+    enemy1, 
+    enemy2,
+    enemy3
+];
+
 // Place the player object in a variable called player
-
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
